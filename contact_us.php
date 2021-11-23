f5c1c38264ec4df97316f78f0d2514c65f39ab17
ThetSuSuKyaw <thetsusukyaw981@gmail.com>
2021-11-23 18:26:06 +0630

    code for contact us

--- contact_us.php
+++ contact_us.php
@@ -1,1 +1,74 @@
-contact_us.php
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<title>Candy May Restaurant</title>
+	<style>
+	#contact1{
+	width: 100%;
+	height: 400px;
+	background: #fff;
+	}
+
+	#lcontact1, #rcontact1{
+		float: left;
+		width: 50%;
+		height: 400px;
+		padding: 20px;
+		padding-left: 30px;
+	}
+
+	#location i, #phone i, #info i{
+		color: #6e0f0c;
+	}
+	</style>
+</head>
+<body>
+	<div id="image">
+		<img src="images/contact.jpg" width="100%" height="400px" style="position: relative;">
+		<h1 id="h1about" class="text-center" style="font-weight: normal;top:25%;left:39%;">Get In Touch</h1>
+		<div id="cover"></div>
+	</div>
+
+	<div id="contact1">
+		<div id="lcontact1">
+			<h1 style="padding-left: -15px;font-weight: normal;margin-bottom: 25px;">CANDY MAY RESTAURANT</h1>
+		<div id="location"><i class="fa fa-map-marker"></i> <p style="text-indent: 30px;display: inline;">  Candy May Restaurant is located at N0.1,<br>&nbsp;&nbsp; Strand Street, Yangon, Myanmar
+			</p>
+		</div><br>
+		<div id="phone"><i class="fa fa-phone"></i><p style="text-indent : 30px;display : inline;">  (415)915-6815
+		</p>
+		</div><br>
+		<div id="info"><i class="fa fa-comment"></i><p style="text-indent: 30px; display: inline;">  info@candymay.com
+		</p>
+		</div>
+		</div>
+		<div id="rcontact1">
+			<div class="mapouter"><div class="gmap_canvas"><iframe width="560" height="350" id="gmap_canvas" src="https://maps.google.com/maps?q=no.1,%20strand%20street,%20yangon&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://123movies-org.net"></a><br><style>.mapouter{position:relative;text-align:right;height:400px;width:480px;}</style></div></div>
+		</div>
+	</div>
+
+	<div id="footer">
+		<div id="fleft">
+			<h2 id="candy" class="h5 text-white">CANDY MAY</h2>
+		<p id="pcandy" class="text-white">Candy May Restaurant, N0.1, Strand Street, Yangon, Myanmar
+			<br>Check in or Delivery:8pm - 8pm
+			<br>(415)915-6815
+			<br>
+			<a href="#">candymay.com</a>
+		</p>
+		</div>
+		<div id="fright">
+				<a href="https://www.facebook.com" style="color: #fff; padding-right: 30px;"><i class="fa fa-facebook-square fa-lg"></i></a>
+				<a href="https://www.instagram.com" style="color: #fff; padding-right: 30px;"><i class="fa fa-instagram fa-lg"></i></a>
+				<a href="https://www.twitter.com" style="color: #fff"><i class="fa fa-twitter fa-lg"></i></a>
+				<p class="mt-3">If you like delicious and great food! Please contact us!</p>
+		</div>
+		<div id="last">&copy; 2021 Grand Design by May & Su</div>
+	</div>
+</body>
+</html>
