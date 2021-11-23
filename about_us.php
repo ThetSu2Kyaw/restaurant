3282e821a697f4684b4f74bf8f5106b50b28becc
ThetSuSuKyaw <thetsusukyaw981@gmail.com>
2021-11-23 17:23:47 +0630

    code for about us

--- about_us.php
+++ about_us.php
@@ -1,1 +1,56 @@
-about_us.php
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<!--<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script> -->
+	<title>Candy May Restaurant</title>
+</head>
+<body>
+	<div id="image">
+		<img src="images/about1.jpg" width="100%" height="400px" style="position: relative;">
+		<h1 id="h1about">ABOUT US</h1>
+		<p class="text-center" id="pabout">CANDY MAY is the best restaurant for you. Food prepared at the same level of quality you're used to experiencing in the comfort of your home.</p>
+		<div id="cover"></div>
+	</div>
+	<div id="div1">
+		<div id="leftChef1">	
+			<h2 class = "mb-2 mt-4" style = "color: #fff; font-weight: normal;">EMILY SARLATTE</h2>
+			<p>A proud native of Oakland, Chef Emily Sarlatte’s love for food began at a very young age. Her fondest memories is shadowing her father in the kitchen, helping him make sausages. Chef Sarlatte’s desire to make the culinary arts her life’s work, and pursue a culinary degree at Laney College. During a stint of time spent studying in Spain, her appreciation and admiration of Spanish cuisine grew to become her work’s focus. Chef Sarlatte enjoys poring over a cookbook or food magazine with a glass of crisp white wine.</p>	
+		</div>
+		<div id="rightChef1">
+			<img src="images/chef.jpg" width="100%" height="400px">	
+		</div>
+	</div>
+	<div id="div2">
+		<div id="leftChef2">
+			<img src="images/chef2.jpg" width="100%" height="400px">	
+		</div>
+		<div id="rightChef2">
+			<h2 class = "mb-2 mt-4" style = "color: #fff; font-weight: normal;">Candy Lion May</h2>
+			<p>A California native, Candy Lion May, was raised in a Spanish household and always loved the culture and cuisine of her family’s birthplace. After being formally trained in the Bay Area, eight unforgettable years in Madrid changed everything for Chef Monleón. Experiencing the beauty of home-cooked paella and the excitement of the traditional tapas stroll, he discovered her life’s passion—creating community through food. Outside the kitchen, she can be found deejaying, watching Spanish fútbol or making paella at Lake Merritt.</p>
+		</div>
+	</div>
+	<div id="footer">
+		<div id="fleft">
+			<h2 id="candy" class="h5 text-white">CANDY MAY</h2>
+		<p id="pcandy" class="text-white">Candy May Restaurant, N0.1, Strand Street, Yangon, Myanmar
+			<br>Check in or Delivery:8pm - 8pm
+			<br>(415)915-6815
+			<br>
+			<a href="#">candymay.com</a>
+		</p>
+		</div>
+		<div id="fright">
+				<a href="https://www.facebook.com" style="color: #fff; padding-right: 30px;"><i class="fa fa-facebook-square fa-lg"></i></a>
+				<a href="https://www.instagram.com" style="color: #fff; padding-right: 30px;"><i class="fa fa-instagram fa-lg"></i></a>
+				<a href="https://www.twitter.com" style="color: #fff"><i class="fa fa-twitter fa-lg"></i></a>
+				<p class="mt-3">If you like delicious and great food! Please contact us!</p>
+		</div>
+		<div id="last">&copy; 2021 Grand Design by May & Su</div>
+	</div>
+</body>
+</html>
