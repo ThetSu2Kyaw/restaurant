152281a8d11e2d09cca1d44ef364d0ef2b7be88c
ThetSuSuKyaw <thetsusukyaw981@gmail.com>
2021-11-25 18:30:53 +0630

    Add admin menu

--- admin/admin_menu.php
+++ admin/admin_menu.php
@@ -1,1 +1,51 @@
-admin_menu.php
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="../css/admin_menu.css">
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+	<title>Candy May Restaurant</title>
+</head>
+<body>
+	<div id="wrap">
+		<h1 class="text-center mb-4">Food Menu</h1>
+		<table id="menu-table">
+		<tr>
+			<th>Category</th>
+			<th>Title</th>
+			<th>Description</th>
+			<th>Price</th>
+			<th>Image</th>
+			<th>Action</th>
+		</tr>
+		<tr>
+			<td>Indian</td>
+			<td>Dosa</td>
+			<td>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</td>
+			<td>1000 Ks</td>
+			<td>
+				<img src="../images/menu1.jpg" width="50%" height="50%">
+			</td>
+			<td>
+				<a href="#" class = "btn btn-sm btn-success">Edit</a> | 
+				<a href="#" class="btn btn-sm btn-danger">Del</a>
+			</td>
+		</tr>
+		<tr>
+			<td>American</td>
+			<td>Burger</td>
+			<td>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</td>
+			<td>1500 Ks</td>
+			<td>
+				<img src="../images/menu1.jpg" width="50%" height="50%">
+			</td>
+			<td>
+				<a href="#" class = "btn btn-sm btn-success">Edit</a> | 
+				<a href="#" class="btn btn-sm btn-danger">Del</a>
+			</td>
+		</tr>
+	</table>
+	</div>
+</body>
+</html>
